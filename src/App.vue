d20dec6355101bdc8dc7bb793c32f964d4adb24d
张天佑 <zhangtianyou@zhangtianyoudeMacBook-Air.local>
2020-05-18 10:41:39 +0800

    项目配置

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>123</h1>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus">
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1
+  color color
+  font-size px2rem(30px)
+</style>
